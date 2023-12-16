cc17d4556ae1c7ead8ce67fdbcb98f916550a350
ccbcsaskatoon <153248087+ccbcsaskatoon@users.noreply.github.com>
2023-12-16 12:58:20 -0600

    Delete TODOAPP CODES directory

--- TODOAPP CODES/backend file/handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports.listTodos = async event => {
-  const todos = ["Wake up at 4AM", "Workout", "Eat breakfast"];
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ todos }, null, 2)
-  };
-};
